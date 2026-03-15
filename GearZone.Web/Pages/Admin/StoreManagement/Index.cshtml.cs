4a06522e18a9482295c571baa7830721432d134a
DamNguyenKhang <nguyeenkhang15112004@gmail.com>
2026-03-15 16:39:27 +0700

    update date range

--- GearZone.Web/Pages/Admin/StoreManagement/Index.cshtml.cs
+++ GearZone.Web/Pages/Admin/StoreManagement/Index.cshtml.cs
@@ -22,11 +22,56 @@ namespace GearZone.Web.Pages.Admin.StoreManagement
         [BindProperty(SupportsGet = true)]
         public StoreApplicationQueryDto Query { get; set; } = new StoreApplicationQueryDto();
 
+        [BindProperty(SupportsGet = true)]
+        public string? DateRangeShortcut { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? DateRange { get; set; }
+
         public PagedResult<StoreApplicationDto> StoreApplications { get; set; } = new() { Items = new(), TotalCount = 0, PageNumber = 1, PageSize = 10 };
         public StoreApplicationStatsDto Stats { get; set; } = new();
 
         public async Task OnGetAsync()
         {
+            if (!string.IsNullOrEmpty(DateRangeShortcut))
+            {
+                var today = DateTime.UtcNow.Date;
+                switch (DateRangeShortcut.ToLower())
+                {
+                    case "today":
+                        Query.StartDate = today;
+                        Query.EndDate = today;
+                        break;
+                    case "week":
+                        Query.StartDate = today.AddDays(-7);
+                        Query.EndDate = today;
+                        break;
+                    case "month":
+                        Query.StartDate = today.AddDays(-30);
+                        Query.EndDate = today;
+                        break;
+                    case "custom":
+                        if (!string.IsNullOrEmpty(DateRange))
+                        {
+                            var dates = DateRange.Split(" to ");
+                            if (dates.Length == 2)
+                            {
+                                if (DateTime.TryParse(dates[0], out var start)) Query.StartDate = start;
+                                if (DateTime.TryParse(dates[1], out var end)) Query.EndDate = end;
+                            }
+                            else if (dates.Length == 1)
+                            {
+                                if (DateTime.TryParse(dates[0], out var start))
+                                {
+                                    Query.StartDate = start;
+                                    Query.EndDate = start;
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+
             if (Query.PageNumber < 1) Query.PageNumber = 1;
             if (Query.PageSize < 1) Query.PageSize = 10;
             Query.ExcludeStatuses = new List<StoreStatus> { StoreStatus.Draft, StoreStatus.Pending, StoreStatus.Rejected };
--- GearZone.Web/Pages/Admin/Transactions/Index.cshtml.cs
+++ GearZone.Web/Pages/Admin/Transactions/Index.cshtml.cs
@@ -20,11 +20,56 @@ namespace GearZone.Web.Pages.Admin.Transactions
         [BindProperty(SupportsGet = true)]
         public PlatformTransactionQuery Query { get; set; } = new();
 
+        [BindProperty(SupportsGet = true)]
+        public string? DateRange { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? DateRangeShortcut { get; set; }
+
         public PagedResult<PlatformTransactionDto> Transactions { get; set; }
         public AdminPlatformTransactionSummaryDto Summary { get; set; }
 
         public async Task OnGetAsync()
         {
+            if (!string.IsNullOrEmpty(DateRangeShortcut))
+            {
+                var today = System.DateTime.UtcNow.Date;
+                switch (DateRangeShortcut.ToLower())
+                {
+                    case "today":
+                        Query.StartDate = today;
+                        Query.EndDate = today;
+                        break;
+                    case "week":
+                        Query.StartDate = today.AddDays(-7);
+                        Query.EndDate = today;
+                        break;
+                    case "month":
+                        Query.StartDate = today.AddDays(-30);
+                        Query.EndDate = today;
+                        break;
+                    case "custom":
+                        if (!string.IsNullOrEmpty(DateRange))
+                        {
+                            var dates = DateRange.Split(" to ");
+                            if (dates.Length == 2)
+                            {
+                                if (System.DateTime.TryParse(dates[0], out var start)) Query.StartDate = start;
+                                if (System.DateTime.TryParse(dates[1], out var end)) Query.EndDate = end;
+                            }
+                            else if (dates.Length == 1)
+                            {
+                                if (System.DateTime.TryParse(dates[0], out var start))
+                                {
+                                    Query.StartDate = start;
+                                    Query.EndDate = start;
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+
             Transactions = await _platformService.GetTransactionsAsync(Query);
             Summary = await _platformService.GetTransactionSummaryAsync(Query);
         }

68a24d7c097cd46a1a145e9f4db00fafa3b51136
DamNguyenKhang <nguyeenkhang15112004@gmail.com>
2026-03-17 02:40:59 +0700

    update order management admin

--- GearZone.Application/Abstractions/Services/IAdminOrderService.cs
+++ GearZone.Application/Abstractions/Services/IAdminOrderService.cs
@@ -7,6 +7,7 @@ namespace GearZone.Application.Abstractions.Services
     public interface IAdminOrderService
     {
         Task<PagedResult<AdminOrderDto>> GetOrdersAsync(AdminOrderQueryDto queryDto);
+        Task<AdminOrderDetailDto?> GetOrderDetailAsync(Guid id);
         Task<AdminOrderStatsDto> GetOrderStatsAsync();
     }
 }
